5eebbfc510db67057869e79a64f2306fe38aab05
João Falcão <jgabfalcao@gmail.com>
2023-12-06 16:32:06 -0300

    feat: Collecting the techrecruiters

--- script.py
+++ script.py
@@ -23,9 +23,9 @@ def main():
 def login(navegador):
   navegador.get("https://www.linkedin.com/home")
   sleep(2)
-  navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('jgabfalcao@gmail.com') #Seu email
+  navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('') #Seu email
   sleep(2)
-  navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('batatinha') #Sua senha
+  navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('') #Sua senha
   sleep(2)
   navegador.find_element('xpath', '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button').click()
   sleep(5)
