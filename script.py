0441f9662211dbecabb63be412d2784034b8f89a
João Falcão <jgabfalcao@gmail.com>
2023-12-06 12:40:59 -0300

    feat: automation is logging into the account

--- script.py
+++ script.py
@@ -1,11 +1,26 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-import time
+from time import sleep
 
-class CookieClicker:
-  def __init__(self):
-    self.link = "https://orteil.dashnet.org/cookieclicker/"
-    self.site_map = {}
-    
-    self.driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
-    self.driver.maximize_window() 
+service = Service(ChromeDriverManager().install())
+
+navegador = webdriver.Chrome(service=service)
+
+navegador.get("https://www.linkedin.com/home")
+
+sleep(2)
+
+navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('') #Seu email
+
+sleep(2)
+
+navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('') #Sua senha
+
+sleep(2)
+
+navegador.find_element('xpath', '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button').click()
+
+sleep(5)
+
