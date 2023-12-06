1b0097ed4bd798a616f30771290f997741c26e66
João Falcão <jgabfalcao@gmail.com>
2023-12-06 13:12:24 -0300

    feat: separating bot functions

--- script.py
+++ script.py
@@ -4,23 +4,24 @@ from selenium.webdriver.chrome.service import Service
 
 from time import sleep
 
-service = Service(ChromeDriverManager().install())
 
-navegador = webdriver.Chrome(service=service)
+def main():
+  service = Service(ChromeDriverManager().install())
+  navegador = webdriver.Chrome(service=service)
+  
+  login(navegador)
+  
+  print("Fim do script")
+  
+def login(navegador):
+  navegador.get("https://www.linkedin.com/home")
+  sleep(2)
+  navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('') #Seu email
+  sleep(2)
+  navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('') #Sua senha
+  sleep(2)
+  navegador.find_element('xpath', '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button').click()
+  sleep(5)
 
-navegador.get("https://www.linkedin.com/home")
-
-sleep(2)
-
-navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('') #Seu email
-
-sleep(2)
-
-navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('') #Sua senha
-
-sleep(2)
-
-navegador.find_element('xpath', '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button').click()
-
-sleep(5)
-
+if __name__ == "__main__":
+    main()
