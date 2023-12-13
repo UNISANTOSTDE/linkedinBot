cd3133a70f3825dd81e07e4082c39629fb74159d
João Falcão <jgabfalcao@gmail.com>
2023-12-13 13:14:12 -0300

    feat: Connecting with 10 Recruiters when profile status is at Conectar

--- script.py
+++ script.py
@@ -15,34 +15,56 @@ def main():
   navegador.maximize_window()
 
   
-  login(navegador)
-  searchTechRecruiters(navegador)
+  # login(navegador)
+  # searchTechRecruiters(navegador)
   
-  
-  
-  
-def login(navegador):
   navegador.get("https://www.linkedin.com/home")
   sleep(2)
-  navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('jgabfalcao@gmail.com') #Seu email
+  navegador.find_element('xpath', '//*[@id="session_key"]').send_keys('') #Seu email
   sleep(2)
-  navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('batatinha') #Sua senha
+  navegador.find_element('xpath', '//*[@id="session_password"]').send_keys('') #Sua senha
   sleep(2)
   navegador.find_element('xpath', '//*[@id="main-content"]/section[1]/div/div/form/div[2]/button').click()
   sleep(5)
-
-
-def searchTechRecruiters(navegador):
+  
   navegador.get('https://www.linkedin.com/search/results/people/?geoUrn=%5B%22106057199%22%5D&keywords=Tech%20Recruiter&origin=FACETED_SEARCH&sid=cE%40')
-  sleep(5)
+  sleep(10)
   
-  html = navegador.page_source
+  divElements = navegador.find_elements(By.CLASS_NAME ,'reusable-search__result-container')
   
-  teste = BeautifulSoup(html, 'html.parser')
+  for elemento in divElements:
+    profileStatus = elemento.find_element(By.CLASS_NAME, 'artdeco-button__text')
+    print(profileStatus.text)
+     
+    if(profileStatus.text == "Conectar"):
+      profileDiv = elemento.find_element(By.CLASS_NAME, 'entity-result__actions')
+      if(profileDiv):
+        profileButton = profileDiv.find_element(By.TAG_NAME, 'button')
+        if(profileButton):
+          profileButton.click()
+          sleep(5)
+          popUpRequest = navegador.find_element(By.CSS_SELECTOR, 'button[aria-label="Enviar agora"]')
+          if(popUpRequest):
+            popUpRequest.click()
+          else:
+            print("Não encontrou o botão de PopUp")  
+        else:
+          print("Não encontrou o botão")
+      else:
+        print("Não encontrou a div")
+      # profileButton.click()
   
-  teste2 = teste.find('ul', attrs={'class': 'reusable-search__entity-result-list list-style-none'}).find_all('li')
+  sleep(500)   
+      
+
   
-  print(teste2)
+  # html = navegador.page_source
+  
+  # teste = BeautifulSoup(html, 'html.parser')
+  
+  # teste2 = teste.find('ul', attrs={'class': 'reusable-search__entity-result-list list-style-none'}).find_all('li')
+  
+  # print(teste2)
   
   
   # elementos = navegador.find_elements(By.CSS_SELECTOR, 'li.reusable-search__result-container')
